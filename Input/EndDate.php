69c48f6af9960e64e290858a55268f620925ce40
Thomas Steur <tsteur@users.noreply.github.com>
2020-07-24 13:38:37 +1200

    fix tests

--- Input/EndDate.php
+++ Input/EndDate.php
@@ -15,58 +15,58 @@ use Piwik\Site;
 
 class EndDate
 {
-	/**
-	 * @internal tests only
-	 * @var null|string
-	 */
-	public $forceMaxEndDate = null;
+    /**
+     * @internal tests only
+     * @var null|string
+     */
+    public $forceMaxEndDate = null;
 
-	/**
-	 * @return Date|null
-	 */
-	public function getMaxEndDate()
-	{
-		// if Matomo for WordPress is used, then the data will be imported into the same site as the data is also being
-		// tracked into by the sounds. So we need to make sure the import ends before Matomo for WordPress was installed
-		// otherwise it would potentially always overwrite already aggregated report data
-		if (method_exists(SettingsServer::class, 'isMatomoForWordPress')
-		    && SettingsServer::isMatomoForWordPress()) {
+    /**
+     * @return Date|null
+     */
+    public function getMaxEndDate()
+    {
+        // if Matomo for WordPress is used, then the data will be imported into the same site as the data is also being
+        // tracked into by the sounds. So we need to make sure the import ends before Matomo for WordPress was installed
+        // otherwise it would potentially always overwrite already aggregated report data
+        if (method_exists(SettingsServer::class, 'isMatomoForWordPress')
+            && SettingsServer::isMatomoForWordPress()) {
 
-			$installDate = null;
-			if (defined('\WpMatomo\Installer::OPTION_NAME_INSTALL_DATE')) {
-				$installDate = get_option(\WpMatomo\Installer::OPTION_NAME_INSTALL_DATE);
-			}
+            $installDate = null;
+            if (defined('\WpMatomo\Installer::OPTION_NAME_INSTALL_DATE')) {
+                $installDate = get_option(\WpMatomo\Installer::OPTION_NAME_INSTALL_DATE);
+            }
 
-			if (empty($installDate)) {
-				// matomo for WordPress was installed before this option was set
-				// we have to make sure there will be an end date otherwise it will always overwrite data
-				// we use site creation date as an alternative
-				$site = new \WpMatomo\Site();
-				$idSite = $site->get_current_matomo_site_id();
-				$installDate = Date::factory(Site::getCreationDateFor($idSite))->subDay(1);
-			} else {
-				// import up to 1 day before original install
-				$installDate = Date::factory($installDate)->subDay(1);
-			}
+            if (empty($installDate)) {
+                // matomo for WordPress was installed before this option was set
+                // we have to make sure there will be an end date otherwise it will always overwrite data
+                // we use site creation date as an alternative
+                $site = new \WpMatomo\Site();
+                $idSite = $site->get_current_matomo_site_id();
+                $installDate = Date::factory(Site::getCreationDateFor($idSite))->subDay(1);
+            } else {
+                // import up to 1 day before original install
+                $installDate = Date::factory($installDate)->subDay(1);
+            }
 
-			return $installDate;
-		}
+            return $installDate;
+        }
 
-		if ($this->forceMaxEndDate) {
-			return Date::factory($this->forceMaxEndDate);
-		}
-	}
+        if ($this->forceMaxEndDate) {
+            return Date::factory($this->forceMaxEndDate);
+        }
+    }
 
-	public function limitMaxEndDateIfNeeded($endDate)
-	{
-		$maxEndDate = $this->getMaxEndDate();
-		// if Matomo for WordPress is used, then the data will be imported into the same site as the data is also being
-		// tracked into by the sounds. So we need to make sure the import ends before Matomo for WordPress was installed
-		// otherwise it would potentially always overwrite already aggregated report data
-		if ($maxEndDate && (!$endDate || Date::factory($endDate)->isLater($maxEndDate))) {
-			$endDate = $maxEndDate->toString();
-		}
+    public function limitMaxEndDateIfNeeded($endDate)
+    {
+        $maxEndDate = $this->getMaxEndDate();
+        // if Matomo for WordPress is used, then the data will be imported into the same site as the data is also being
+        // tracked into by the sounds. So we need to make sure the import ends before Matomo for WordPress was installed
+        // otherwise it would potentially always overwrite already aggregated report data
+        if ($maxEndDate && (!$endDate || Date::factory($endDate)->isLater($maxEndDate))) {
+            $endDate = $maxEndDate->toString();
+        }
 
-		return $endDate;
-	}
+        return $endDate;
+    }
 }
--- tests/Unit/Input/EndDateTest.php
+++ tests/Unit/Input/EndDateTest.php
@@ -18,43 +18,43 @@ class EndDateTest extends \PHPUnit_Framework_TestCase
 
     public function test_getMaxEndDate()
     {
-    	$maxEndDate = new EndDate();
-    	$this->assertNull($maxEndDate->getMaxEndDate());
+        $maxEndDate = new EndDate();
+        $this->assertNull($maxEndDate->getMaxEndDate());
     }
 
     public function test_getMaxEndDate_forcedDate()
     {
-    	$maxEndDate = new EndDate();
-    	$maxEndDate->forceMaxEndDate = '2020-03-19';
-    	$this->assertSame('2020-03-19', $maxEndDate->getMaxEndDate()->toString());
+        $maxEndDate = new EndDate();
+        $maxEndDate->forceMaxEndDate = '2020-03-19';
+        $this->assertSame('2020-03-19', $maxEndDate->getMaxEndDate()->toString());
     }
 
     public function test_limitMaxEndDateIfNeeded()
     {
-    	$maxEndDate = new EndDate();
-    	$this->assertSame('', $maxEndDate->limitMaxEndDateIfNeeded(''));
-    	$this->assertSame('2019-01-02', $maxEndDate->limitMaxEndDateIfNeeded('2019-01-02'));
+        $maxEndDate = new EndDate();
+        $this->assertSame('', $maxEndDate->limitMaxEndDateIfNeeded(''));
+        $this->assertSame('2019-01-02', $maxEndDate->limitMaxEndDateIfNeeded('2019-01-02'));
     }
 
     public function test_limitMaxEndDateIfNeeded_noNeedToLimit()
     {
-    	$maxEndDate = new EndDate();
-	    $maxEndDate->forceMaxEndDate = '2020-03-19';
-    	$this->assertSame('2019-03-19', $maxEndDate->limitMaxEndDateIfNeeded('2019-01-02'));
+        $maxEndDate = new EndDate();
+        $maxEndDate->forceMaxEndDate = '2020-03-19';
+        $this->assertSame('2019-01-02', $maxEndDate->limitMaxEndDateIfNeeded('2019-01-02'));
     }
 
     public function test_limitMaxEndDateIfNeeded_whenNoEndDateSetShouldLimit()
     {
-    	$maxEndDate = new EndDate();
-	    $maxEndDate->forceMaxEndDate = '2020-03-19';
-    	$this->assertSame('2019-03-19', $maxEndDate->limitMaxEndDateIfNeeded(''));
+        $maxEndDate = new EndDate();
+        $maxEndDate->forceMaxEndDate = '2020-03-19';
+        $this->assertSame('2020-03-19', $maxEndDate->limitMaxEndDateIfNeeded(''));
     }
 
     public function test_limitMaxEndDateIfNeeded_shouldLimitWhenMaxDateOlder()
     {
-    	$maxEndDate = new EndDate();
-	    $maxEndDate->forceMaxEndDate = '2018-03-19';
-    	$this->assertSame('2018-03-19', $maxEndDate->limitMaxEndDateIfNeeded('2019-01-02'));
+        $maxEndDate = new EndDate();
+        $maxEndDate->forceMaxEndDate = '2018-03-19';
+        $this->assertSame('2018-03-19', $maxEndDate->limitMaxEndDateIfNeeded('2019-01-02'));
     }
 
 }
