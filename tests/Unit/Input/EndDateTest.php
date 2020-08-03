b93386f81efdd0222a7298b25bae7932a0bf3735
diosmosis <diosmosis@users.noreply.github.com>
2020-08-03 03:58:15 -0700

    3.x phpunit

--- tests/Unit/Input/EndDateTest.php
+++ tests/Unit/Input/EndDateTest.php
@@ -9,83 +9,94 @@
 
 namespace Piwik\Plugins\GoogleAnalyticsImporter\tests\Unit\Google;
 
-use Piwik\Metrics;
-use Piwik\Plugins\GoogleAnalyticsImporter\Google\GoogleMetricMapper;
+use Piwik\Date;
 use Piwik\Plugins\GoogleAnalyticsImporter\Input\EndDate;
 
 class EndDateTest extends \PHPUnit_Framework_TestCase
 {
     public function test_getMaxEndDate_withConfigSectionButNoValue()
     {
-        $mockConfig = new \stdClass();
+        $mockConfig = $this->getMockConfig([]);
         $endDate = new EndDate($mockConfig);
         $this->assertNull($endDate->getMaxEndDate());
 
-        $mockConfig = new \stdClass();
-        $mockConfig->GoogleAnalyticsImporter = [];
+        $mockConfig = $this->getMockConfig([]);
         $endDate = new EndDate($mockConfig);
         $this->assertNull($endDate->getMaxEndDate());
     }
 
     public function test_getMaxEndDate_withConfigSectionWithValue()
     {
-        $mockConfig = new \stdClass();
-        $mockConfig->GoogleAnalyticsImporter = [
+        $mockConfig = $this->getMockConfig([
             EndDate::CONFIG_NAME => 'today',
-        ];
+        ]);
         $endDate = new EndDate($mockConfig);
         $this->assertEquals(Date::factory('today'), $endDate->getMaxEndDate());
     }
 
     public function test_getMaxEndDate_withConfigSectionWithValue_thatIsExplicitDate()
     {
-        $mockConfig = new \stdClass();
-        $mockConfig->GoogleAnalyticsImporter = [
+        $mockConfig = $this->getMockConfig([
             EndDate::CONFIG_NAME => '2023-05-06',
-        ];
+        ]);
         $endDate = new EndDate($mockConfig);
         $this->assertEquals(Date::factory('2023-05-06'), $endDate->getMaxEndDate());
     }
 
     public function test_getMaxEndDate()
     {
-        $maxEndDate = new EndDate(null);
+        $maxEndDate = new EndDate($this->getMockConfig([]));
         $this->assertNull($maxEndDate->getMaxEndDate());
     }
 
     public function test_getMaxEndDate_forcedDate()
     {
-        $maxEndDate = new EndDate(null);
+        $maxEndDate = new EndDate($this->getMockConfig([]));
         $maxEndDate->forceMaxEndDate = '2020-03-19';
         $this->assertSame('2020-03-19', $maxEndDate->getMaxEndDate()->toString());
     }
 
     public function test_limitMaxEndDateIfNeeded()
     {
-        $maxEndDate = new EndDate(null);
+        $maxEndDate = new EndDate($this->getMockConfig([]));
         $this->assertSame('', $maxEndDate->limitMaxEndDateIfNeeded(''));
         $this->assertSame('2019-01-02', $maxEndDate->limitMaxEndDateIfNeeded('2019-01-02'));
     }
 
     public function test_limitMaxEndDateIfNeeded_noNeedToLimit()
     {
-        $maxEndDate = new EndDate(null);
+        $maxEndDate = new EndDate($this->getMockConfig([]));
         $maxEndDate->forceMaxEndDate = '2020-03-19';
         $this->assertSame('2019-01-02', $maxEndDate->limitMaxEndDateIfNeeded('2019-01-02'));
     }
 
     public function test_limitMaxEndDateIfNeeded_whenNoEndDateSetShouldLimit()
     {
-        $maxEndDate = new EndDate(null);
+        $maxEndDate = new EndDate($this->getMockConfig([]));
         $maxEndDate->forceMaxEndDate = '2020-03-19';
         $this->assertSame('2020-03-19', $maxEndDate->limitMaxEndDateIfNeeded(''));
     }
 
     public function test_limitMaxEndDateIfNeeded_shouldLimitWhenMaxDateOlder()
     {
-        $maxEndDate = new EndDate(null);
+        $maxEndDate = new EndDate($this->getMockConfig([]));
         $maxEndDate->forceMaxEndDate = '2018-03-19';
         $this->assertSame('2018-03-19', $maxEndDate->limitMaxEndDateIfNeeded('2019-01-02'));
+    }
+
+    private function getMockConfig(array $gaConfig)
+    {
+        $mock = $this->getMockBuilder(\Piwik\Config::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['__get'])
+            ->getMock();
+        $mock->method('__get')->willReturnCallback(function ($section) use ($gaConfig) {
+            if ($section != 'GoogleAnalyticsImporter') {
+                return [];
+            }
+            return $gaConfig;
+        });
+        return $mock;
     }
 
 }
