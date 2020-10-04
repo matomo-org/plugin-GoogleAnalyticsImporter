139483c4d650bf2e1ee48e46499fd482eff4551d
diosmosis <diosmosis@users.noreply.github.com>
2020-10-04 11:29:50 -0700

    fixing build

--- tests/Fixtures/ImportedFromGoogle.php
+++ tests/Fixtures/ImportedFromGoogle.php
@@ -91,7 +91,8 @@ class ImportedFromGoogle extends Fixture
         DbHelper::getTablesInstalled(true);
 
         $cronArchive = new CronArchive();
-        $cronArchive->invalidateArchivedReportsForSitesThatNeedToBeArchivedAgain();
+        $cronArchive->invalidateArchivedReportsForSitesThatNeedToBeArchivedAgain(1);
+        $cronArchive->invalidateArchivedReportsForSitesThatNeedToBeArchivedAgain(2);
     }
 
     private function aggregateForYear()
