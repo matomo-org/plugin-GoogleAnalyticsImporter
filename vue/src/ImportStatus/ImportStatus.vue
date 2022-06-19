123f5ee3caefb7864cd6a8a9aaaefb06afb868a4
diosmosis <diosmosis@users.noreply.github.com>
2022-06-19 14:49:21 -0700

    remove check for piwik-field

--- vue/src/ImportStatus/ImportStatus.vue
+++ vue/src/ImportStatus/ImportStatus.vue
@@ -260,11 +260,6 @@ export default defineComponent({
       return function tooltipContent(this: HTMLElement) {
         const $this = $(this);
 
-        if ($this.attr('piwik-field') === '') {
-          // do not show it for form fields
-          return '';
-        }
-
         const title = $(this).attr('title') || '';
         return window.vueSanitize(title.replace(/\n/g, '<br />'));
       };
