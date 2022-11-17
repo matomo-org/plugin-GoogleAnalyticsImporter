c11b1e98ab116f69e6e51a886f1a245ea559d01e
Dileep Ratnayake <dileep_ratnayake@hotmail.com>
2022-11-17 14:35:00 +1300

    Add default value

--- Exceptions/CloudApiQuotaExceeded.php
+++ Exceptions/CloudApiQuotaExceeded.php
@@ -4,7 +4,7 @@ namespace Piwik\Plugins\GoogleAnalyticsImporter\Exceptions;
 
 class CloudApiQuotaExceeded extends \RuntimeException
 {
-    public function __construct($limit)
+    public function __construct($limit = 0)
     {
         parent::__construct('Daily cloud rate limit reached, try again tomorrow. (Note: GA by default sets a daily limit on the number'
             . ' of API requests made each day. This has been prorated to  ' . $limit . 'requests to your account. 
