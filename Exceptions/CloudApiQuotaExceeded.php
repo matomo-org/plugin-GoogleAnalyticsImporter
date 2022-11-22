6d1934452abef60a7ceb2f52b716dc3e6a6b7cec
Dileep Ratnayake <dileep_ratnayake@hotmail.com>
2022-11-22 15:57:56 +1300

    added missing space

--- Exceptions/CloudApiQuotaExceeded.php
+++ Exceptions/CloudApiQuotaExceeded.php
@@ -7,7 +7,7 @@ class CloudApiQuotaExceeded extends \RuntimeException
     public function __construct($limit = 0)
     {
         parent::__construct('Daily cloud rate limit reached, try again tomorrow. (Note: GA by default sets a daily limit on the number'
-            . ' of API requests made each day. This has been prorated to  ' . $limit . 'requests to your account. 
+            . ' of API requests made each day. This has been prorated to  ' . $limit . ' requests to your account. 
             This limit is prorated for each Matomo cloud customer. It looks like you\'ve reached your limit. Continue the import tomorrow.)');
     }
 }
