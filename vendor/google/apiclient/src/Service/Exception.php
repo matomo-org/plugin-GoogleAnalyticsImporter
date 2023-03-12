ca0ba4aefcd071a32fba6b4edc012c7ee388a25b
Jacob Ransom <jacob@innocraft.com>
2023-03-13 10:01:27 +1300

    Revert "Updating dependent libraries to get rid of PHP 8.2 deprecation errors"
    This reverts commit 09f1ea4c685878a606d1e741b4ba89f06dee62fc.

--- vendor/google/apiclient/src/Service/Exception.php
+++ vendor/google/apiclient/src/Service/Exception.php
@@ -21,53 +21,51 @@ use Google\Exception as GoogleException;
 
 class Exception extends GoogleException
 {
-    /**
-     * Optional list of errors returned in a JSON body of an HTTP error response.
-     */
-    protected $errors = [];
+  /**
+   * Optional list of errors returned in a JSON body of an HTTP error response.
+   */
+  protected $errors = array();
 
-    /**
-     * Override default constructor to add the ability to set $errors and a retry
-     * map.
-     *
-     * @param string $message
-     * @param int $code
-     * @param Exception|null $previous
-     * @param array<array<string,string>>|null $errors List of errors returned in an HTTP
-     * response or null.  Defaults to [].
-     */
-    public function __construct(
-        $message,
-        $code = 0,
-        Exception $previous = null,
-        $errors = []
-    ) {
-        if (version_compare(PHP_VERSION, '5.3.0') >= 0) {
-            parent::__construct($message, $code, $previous);
-        } else {
-            parent::__construct($message, $code);
-        }
-
-        $this->errors = $errors;
+  /**
+   * Override default constructor to add the ability to set $errors and a retry
+   * map.
+   *
+   * @param string $message
+   * @param int $code
+   * @param \Exception|null $previous
+   * @param [{string, string}] errors List of errors returned in an HTTP
+   * response.  Defaults to [].
+   */
+  public function __construct(
+      $message,
+      $code = 0,
+      Exception $previous = null,
+      $errors = array()
+  ) {
+    if (version_compare(PHP_VERSION, '5.3.0') >= 0) {
+      parent::__construct($message, $code, $previous);
+    } else {
+      parent::__construct($message, $code);
     }
 
-    /**
-     * An example of the possible errors returned.
-     *
-     * [
-     *   {
-     *     "domain": "global",
-     *     "reason": "authError",
-     *     "message": "Invalid Credentials",
-     *     "locationType": "header",
-     *     "location": "Authorization",
-     *   }
-     * ]
-     *
-     * @return array<array<string,string>>|null List of errors returned in an HTTP response or null.
-     */
-    public function getErrors()
-    {
-        return $this->errors;
-    }
+    $this->errors = $errors;
+  }
+
+  /**
+   * An example of the possible errors returned.
+   *
+   * {
+   *   "domain": "global",
+   *   "reason": "authError",
+   *   "message": "Invalid Credentials",
+   *   "locationType": "header",
+   *   "location": "Authorization",
+   * }
+   *
+   * @return [{string, string}] List of errors return in an HTTP response or [].
+   */
+  public function getErrors()
+  {
+    return $this->errors;
+  }
 }
