04f84623fa3ac228fadf8039877b1040c19980e0
sgiehl <stefan@matomo.org>
2023-09-19 14:17:48 +0200

    Refactor UI tests into one suite and update test cases

--- tests/UI/EmptySite_GA_spec.js
+++ tests/UI/EmptySite_GA_spec.js
@@ -10,16 +10,58 @@
 describe("EmptySite_GA", function () {
   this.timeout(0);
 
-  this.fixture = "Piwik\\Plugins\\GoogleAnalyticsImporter\\tests\\Fixtures\\EmptySiteWithSiteContentDetectionGA";
+  this.fixture = "Piwik\\Tests\\Fixtures\\EmptySite";
 
   const generalParams = 'idSite=1&period=day&date=2010-01-03';
 
-  it('should show no data screen with GA import and with ga3 offset banner', async function () {
+  before(function () {
+    testEnvironment.detectedContentDetections = [];
+    testEnvironment.connectedConsentManagers = [];
+    testEnvironment.save();
+  });
+
+  after(function () {
+    // unset all detections so fake class is no longer used
+    delete testEnvironment.detectedContentDetections;
+    delete testEnvironment.connectedConsentManagers;
+    testEnvironment.save();
+  });
+
+  it('should show no data screen with GA import recommended', async function () {
+    testEnvironment.detectedContentDetections = ['GoogleAnalytics3', 'Cloudflare'];
+    testEnvironment.connectedConsentManagers = [];
+    testEnvironment.save();
+
     const urlToTest = "?" + generalParams + "&module=CoreHome&action=index";
     await page.goto(urlToTest);
 
     const pageElement = await page.$('.page');
-    expect(await pageElement.screenshot()).to.matchImage('emptySiteDashboard');
+    expect(await pageElement.screenshot()).to.matchImage('list');
   });
 
+  it('should show import details with ga3 offset banner', async function () {
+    await page.evaluate(() => $('#start-tracking-detection a[href="#googleanalyticsimporter"]')[0].click());
+
+    const pageElement = await page.$('.page');
+    expect(await pageElement.screenshot()).to.matchImage('details_ga3');
+  });
+
+  it('should show no data screen with GA import recommended', async function () {
+    testEnvironment.detectedContentDetections = ['GoogleAnalytics4', 'Cloudflare'];
+    testEnvironment.connectedConsentManagers = [];
+    testEnvironment.save();
+
+    const urlToTest = "?" + generalParams + "&module=CoreHome&action=index";
+    await page.goto(urlToTest);
+
+    const pageElement = await page.$('.page');
+    expect(await pageElement.screenshot()).to.matchImage('list');
+  });
+
+  it('should show import details', async function () {
+    await page.evaluate(() => $('#start-tracking-detection a[href="#googleanalyticsimporter"]')[0].click());
+
+    const pageElement = await page.$('.page');
+    expect(await pageElement.screenshot()).to.matchImage('details_ga4');
+  });
 });
