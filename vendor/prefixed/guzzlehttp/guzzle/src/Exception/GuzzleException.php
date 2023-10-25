361f60826edbc3c0825e0aaf6fb329190acc3df6
Jacob Ransom <jacob@innocraft.com>
2023-10-25 15:33:31 +1300

    Revert "Initial attempt"
    This reverts commit 97c2ee00da3e855297583fda0dfc818a0292ddda.

--- vendor/prefixed/guzzlehttp/guzzle/src/Exception/GuzzleException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace GuzzleHttp\Exception;
-
-use Matomo\Dependencies\GoogleAnalyticsImporter\Psr\Http\Client\ClientExceptionInterface;
-interface GuzzleException extends ClientExceptionInterface
-{
-}
