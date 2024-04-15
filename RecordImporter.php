0999fe0e7425fe06645687abb09904a91701383d
Altamash Shaikh <altu9594@gmail.com>
2024-04-15 12:46:42 +0530

    Fixes failing testcase

--- RecordImporter.php
+++ RecordImporter.php
@@ -51,7 +51,7 @@ abstract class RecordImporter
     {
         return \true;
     }
-    public function abstract importRecords(Date $day);
+    public abstract function importRecords(Date $day);
     public function setRecordInserter(\Piwik\Plugins\GoogleAnalyticsImporter\RecordInserter $recordInserter)
     {
         $this->recordInserter = $recordInserter;
