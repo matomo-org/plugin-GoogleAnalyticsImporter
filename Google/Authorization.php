ad2e43bc6105bfc28a8ec5fd12821cdbc3ef457b
Altamash Shaikh <altu9594@gmail.com>
2025-05-28 07:50:01 +0530

    append index.php to fallback url only when not present

--- Google/Authorization.php
+++ Google/Authorization.php
@@ -137,7 +137,11 @@ class Authorization
     private function getValidUri($uris)
     {
         $validUri = Url::getCurrentUrlWithoutQueryString() . '?module=GoogleAnalyticsImporter&action=processAuthCode';
-        $validUriFallback = SettingsPiwik::getPiwikUrl() . 'index.php?module=GoogleAnalyticsImporter&action=processAuthCode'; // Some MWP installs was not working as expected when using Url::getCurrentUrlWithoutQueryString()
+        $settingURL = SettingsPiwik::getPiwikUrl();
+        if (stripos($settingURL, 'index.php') === false) {
+            $settingURL .= 'index.php';
+        }
+        $validUriFallback = $settingURL . '?module=GoogleAnalyticsImporter&action=processAuthCode'; // Some MWP installs was not working as expected when using Url::getCurrentUrlWithoutQueryString()
         foreach ($uris as $uri) {
             if (stripos($uri, $validUri) !== \false || stripos($uri, $validUriFallback) !== \false) {
                 return $uri;
